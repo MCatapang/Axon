13a8c16bb1e668361c6c8462a27fdd55496f7f70
MCatapang <maocatap@gmail.com>
2022-06-28 12:47:14 -0700

    Kept opacity constant

--- wwwroot/script/script.js
+++ wwwroot/script/script.js
@@ -1,12 +1,10 @@
 function scrollFunction() {
     if(document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         document.getElementById("navBar").style.height = "100px";
-        document.getElementById("navBar").style.opacity = "90%";
         document.getElementById("navLinkContainer").style.transform = "translate(500px)";
         document.getElementById("navLogo").style.fontSize = "50px";
     } else {
         document.getElementById("navBar").style.height = "125px";
-        document.getElementById("navBar").style.opacity = "100%";
         document.getElementById("navLinkContainer").style.transform = "translate(0px)";
         document.getElementById("navLogo").style.fontSize = "75px";
     }

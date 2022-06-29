bda8df9179e791670a721325f3f9cb05e83f225a
MCatapang <maocatap@gmail.com>
2022-06-29 11:07:22 -0700

    Added logo styling

--- wwwroot/script/script.js
+++ wwwroot/script/script.js
@@ -3,10 +3,12 @@ function scrollFunction() {
         document.getElementById("navBar").style.height = "75px";
         document.getElementById("navLinkContainer").style.transform = "translate(500px)";
         document.getElementById("navLogo").style.fontSize = "25px";
+        document.getElementById("navLogo").style.color = "#0095ff";
     } else {
         document.getElementById("navBar").style.height = "125px";
         document.getElementById("navLinkContainer").style.transform = "translate(0px)";
         document.getElementById("navLogo").style.fontSize = "75px";
+        document.getElementById("navLogo").style.color = "#ffffff";
     }
 }
 window.onscroll = function() {

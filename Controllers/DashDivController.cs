76e8a28dbffac03c6d6864cb8db383c674d7240b
MCatapang <maocatap@gmail.com>
2022-07-08 14:10:52 -0700

    Started utilization of DashDivController for Views

--- Controllers/DashDivController.cs
+++ Controllers/DashDivController.cs
@@ -10,6 +10,35 @@ namespace Axon.Controllers;
 
 public class DashDivController : Controller
 {
+    private MyContext _context;
+
+    public DashDivController(MyContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("/dashboard")]
+    public IActionResult Dashboard()
+    {
+        int? employeeID = HttpContext.Session.GetInt32("EmployeeID");
+        HttpContext.Session.SetString("ActiveLink", "Dashboard");
+        Employee? employee = _context.Employees
+            .FirstOrDefault(e => e.EmployeeID == employeeID);
+
+        if(employee == null) 
+        {
+            return RedirectToAction("Home", "HomeController");
+        }
+
+        ViewBag.AllPatients = _context.Patients
+            .Where(p => p.Facility == employee.Facility)
+            .ToList();
+        ViewBag.Shift = _context.Employees
+            .Where(e => e.Facility == employee.Facility)
+            .ToList();
+        return View("/Views/DashDiv/Dashboard.cshtml", employee);
+    }
+
     [HttpGet("/logout")]
     public IActionResult Logout()
     {
